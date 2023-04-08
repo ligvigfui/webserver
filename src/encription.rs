55269df82cd0a7ddbe53bdd88213fc77fc2fde5e
ligvigfui <ligvigfui@gmail.com>
2023-04-08 16:00:08 +0200

    deleted most of the project but it looks better

---
+++ src/encription.rs
@@ -0,0 +1,19 @@
+use std::{sync::{Arc, Mutex}};
+
+use webserver::{User, CustomResult};
+
+
+
+pub fn login(mut users: Arc<Vec<Mutex<User>>>, credentials: (String, String, String)) -> CustomResult {
+    //credentials: (email, password, MAC)
+    let (email, password, session_id) = credentials;
+    for user in &mut users.iter() {
+        let mut user = user.lock().unwrap();
+        if user.email == email && user.password == password {
+            return user.login(session_id);
+        } else {
+            return CustomResult::Wc
+        }
+    }
+    CustomResult::Ok
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use hash::get_response;
 use webserver::ThreadPool;
-use crate::lib::User;
+use lib::User;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -11,9 +11,10 @@ use std::thread;
 use std::time::Duration;
 mod lib;
 mod hash;
+use std::process::{Command, exit};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
     let pool = ThreadPool::new(4);
     //todo load in users
     let users = Arc::new(vec![
@@ -36,64 +37,87 @@ fn handle_connection(mut stream: TcpStream, users: Arc<Vec<Mutex<User>>>) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-    let neptun = b"GET /neptun_fos HTTP/1.1\r\n";
-    let login_neptun = b"PUT /login_neptun_fos HTTP/1.1\r\n";
+    let starts_with = buffer.split(|&x| x == b'\r').next().unwrap();
 
+    match starts_with {
+        b"GET / HTTP/1.1" => {
+            println!("Handling root");
+            default_handle_page_return(&mut stream, "200 OK", "hello.html");},
+        b"GET /sleep HTTP/1.1" => {
+            println!("Sleeping for 5 seconds");
+            thread::sleep(Duration::from_secs(5));
+            default_handle_page_return(&mut stream, "200 OK", "hello.html");},
+        b"GET /neptun_fos HTTP/1.1" => {
+            println!("Handling neptun_fos");
+            default_handle_page_return(&mut stream, "200 OK", "neptun_fos.html");},
+        b"POST /neptun_fos/login HTTP/1.1" => {
+            println!("Handling neptun_fos login");
+            handle_neptun_fos_login(&mut stream, buffer, users);},
+        b"GET /debug HTTP/1.1" => {
+            println!("Providing debug info");
+            handle_debug(&mut stream, buffer);},
+        _ => {
+            println!("404");
+            default_handle_page_return(&mut stream, "404 NOT FOUND", "404.html");}
+    }
+}
 
-    if buffer.starts_with(login_neptun) {
-        let (status, string) = handle_login(buffer, users);
-        let response = format!(
-            "HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
-            status,
-            string.len(),
-            string
-        );
-        stream.write_all(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-        return;
-    } 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(neptun) {
-        ("HTTP/1.1 200 OK", "neptun_fos.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
+fn default_handle_page_return(stream: &mut TcpStream, status: &str, html_name: &str){
+    let contents = fs::read_to_string("pages\\".to_owned() + html_name).unwrap();
+    default_handle(stream, status, &contents);
+}
 
-    let contents = fs::read_to_string("pages\\".to_owned() + filename).unwrap();
-
+fn default_handle(stream: &mut TcpStream , status: &str, contents: &str) {
     let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
+        "HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
         contents.len(),
         contents
     );
-
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();   
+}
+
+fn handle_neptun_fos_login(stream: &mut TcpStream , buffer: [u8; 1024], users: Arc<Vec<Mutex<User>>>){
+
+}
+
+fn handle_debug(stream: &mut TcpStream , buffer: [u8; 1024]){
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
     stream.flush().unwrap();
 }
 
-fn handle_login(buffer: [u8; 1024], users: Arc<Vec<Mutex<User>>>) -> (String, String){
-    //return: status, response
-    // convert the buffer to a string
-    let string = match String::from_utf8(buffer.to_vec()) {
-        Ok(v) => v,
-        Err(e) => {
-            println!("Invalid UTF-8 sequence: {}", e);
-            return ("400 Bad Request".to_string(), "Invalid UTF-8 sequence".to_string());
-        }
-    };
+fn update() {
+    // Clone the repository
+    let repo_url = "https://github.com/ligvigfui/repo.git";
+    let repo_dir = "repo";
+    if let Err(err) = Command::new("git").args(&["clone", repo_url, repo_dir]).status() {
+        eprintln!("Failed to clone repository: {}", err);
+    }
 
+    // Rename the old executable
+    let old_exe_path = std::env::current_exe().unwrap();
+    let old_exe_name = old_exe_path.file_name().unwrap();
+    let old_exe_backup = old_exe_path.with_file_name(format!("{}_old{}", old_exe_name.to_str().unwrap(), old_exe_path.extension().unwrap_or_default().to_str().unwrap()));
+    if let Err(err) = std::fs::rename(&old_exe_path, &old_exe_backup) {
+        eprintln!("Failed to rename old executable: {}", err);
+        exit(1);
+    }
 
+    // Build the latest executable
+    if let Err(err) = Command::new("cargo").args(&["build", "--release"]).current_dir(repo_dir).status() {
+        eprintln!("Failed to build latest executable: {}", err);
+        exit(1);
+    }
 
-    println!("Users: {:?}", users);
-    
-    // do something with the credentials
-    get_response(&string, users)
-    
+    // Run the latest executable
+    let new_exe_path = format!("{}/target/release/main", repo_dir);
+    if let Err(err) = Command::new(&new_exe_path).spawn() {
+        eprintln!("Failed to run latest executable: {}", err);
+        exit(1);
+    }
+
+    // Close this instance of the program
+    exit(0);
 }
