5fe2f7b336d849a2748bdc84b6572d0520822ae4
ligvigfui <ligvigfui@gmailcom>
2023-09-24 08:20:54 +0200

    added initial deploy funciton

--- src/server_functions/mod.rs
+++ src/server_functions/mod.rs
@@ -1,2 +1,26 @@
+use std::process::Command;
+
 pub mod routing;
 pub mod handling;
+
+pub fn deploy() {
+    // run yarn build
+    let mut child = Command::new("yarn")
+        .arg("build")
+        .current_dir("pages/vue")
+        .spawn()
+        .expect("Failed to run yarn build");
+
+    let mut child2 = Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .spawn()
+        .expect("Failed to run cargo run");
+
+    child.wait().expect("yarn build failed");
+    child2.wait().expect("cargo build failed");
+    
+    println!("yarn build finished")
+    // replace vue page references
+    
+}
