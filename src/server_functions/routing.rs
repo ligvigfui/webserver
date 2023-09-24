5616148353bda4983137353f68b531a3bd56c630
ligvigfui <ligvigfui@gmailcom>
2023-09-24 08:21:04 +0200

    Revert "added initial deploy funciton"
    This reverts commit 853f368bbc8132bb726c7361cc5144f2a447614f.

--- src/server_functions/routing.rs
+++ src/server_functions/routing.rs
@@ -1,5 +1,5 @@
 use std::net::TcpStream;
-use crate::{*, server_functions::deploy};
+use crate::*;
 
 
 pub fn routing(stream: &mut TcpStream, request: Request, users: Arc<Vec<Mutex<User>>>){
@@ -25,7 +25,6 @@ pub fn routing(stream: &mut TcpStream, request: Request, users: Arc<Vec<Mutex<Us
         (_, "/favicon.ico") => handle_image(stream, "/favicon.ico"),
         (M::GET, "/") => handle_page_return(stream, "200 OK", None, "/index.html"),
         (M::GET, "/debug") => handle_debug(stream, request),
-        (M::GET, "/deploy") => deploy(),
         _ => {
             println!("404 Page not found");
             handle_page_return(stream, "404 NOT FOUND", None,
